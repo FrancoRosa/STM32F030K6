6c4da7d7bb62ae914823768d04aab30cba4adc94
Franco Rosa <km115.franco@gmail.com>
2018-01-13 08:59:28 -0500

    Add files via upload

---
+++ PLC_F030/variant.h
@@ -0,0 +1,171 @@
+/*
+ *******************************************************************************
+ * Copyright (c) 2017, STMicroelectronics
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of STMicroelectronics nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************
+ */
+
+#ifndef _VARIANT_ARDUINO_STM32_
+#define _VARIANT_ARDUINO_STM32_
+
+/*----------------------------------------------------------------------------
+ *        Headers
+ *----------------------------------------------------------------------------*/
+
+#include "pins_arduino.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif // __cplusplus
+
+/*----------------------------------------------------------------------------
+ *        Pins
+ *----------------------------------------------------------------------------*/
+extern const PinName digitalPin[];
+
+// Enum defining pin names to match digital pin number --> Dx
+// !!!
+// !!! Copy the digitalPin[] array in variant.cpp
+// !!! and remove all '_': PX_n --> PXn
+// !!! For NC, suffix by _x where x is the number of NC:
+// !!!   NC_1, NC_2,...
+// !!! For duplicated pin name, suffix by _x where x is the number of pin:
+// !!! PA7, PA7_2, PA7_3,...
+enum {
+  PA0,  //D0
+  PA1,  //D1
+  PA2,  //D2
+  PA3,  //D3
+  PA4,  //D4
+  PA5,  //D5
+  PA6,  //D6
+  PA7,  //D7
+  PB0,  //D8
+  PB1,  //D9
+  PA8,  //D10
+  PA9,  //D11
+  PA10, //D12
+  PA11, //D13
+  PA12, //D14
+  PA13, //D15
+  PA14, //D16
+  PA15, //D17
+  PB3,  //D18
+  PB4,  //D19
+  PB5,  //D20
+  PB6,  //D21
+  PB7,  //D22
+  PEND
+};
+// Enum defining Arduino style alias for analog pin number --> Ax
+// !!!
+// !!! It must be aligned with the number of analog PinName
+// !!! defined in digitalPin[] array in variant.cpp
+// !!!
+enum {
+  A_START_AFTER = D0, // pin number preceding A0
+  A0,  A1,  A2,  A3,  A4,  A5,  A6,  A7,  A8,
+  AEND
+};
+
+//ADC resolution is 12bits
+#define ADC_RESOLUTION          12
+#define DACC_RESOLUTION         12
+
+//PWR resolution
+#define PWM_RESOLUTION          8
+#define PWM_FREQUENCY           1000
+#define PWM_MAX_DUTY_CYCLE      255
+
+//On-board LED pin number
+#define LED_BUILTIN             D0
+#define LED_GREEN               LED_BUILTIN
+
+//On-board user button
+#define USER_BTN                D1
+
+
+//SPI definitions
+#define SS                      10 // Default for Arduino connector compatibility
+#define MOSI                    11 // Default for Arduino connector compatibility
+#define MISO                    12 // Default for Arduino connector compatibility
+#define SCK                     13 // Default for Arduino connector compatibility
+
+//I2C Definitions
+#define SDA                     14 // Default for Arduino connector compatibility
+#define SCL                     15 // Default for Arduino connector compatibility
+
+//Timer Definitions
+//Do not use timer used by PWM pins when possible. See PinMap_PWM in PeripheralPins.c
+#define TIMER_TONE              TIM14
+#define TIMER_UART_EMULATED     TIM16
+
+//Do not use basic timer: OC is required
+#define TIMER_SERVO             TIM17  //TODO: advanced-control timers don't work
+
+// UART Definitions
+// Define here Serial instance number to map on Serial generic name
+#define SERIAL_UART_INSTANCE    1 //ex: 2 for Serial2 (USART2)
+// DEBUG_UART could be redefined to print on another instance than 'Serial'
+//#define DEBUG_UART              ((USART_TypeDef *) U(S)ARTX) // ex: USART3
+
+// UART Emulation (uncomment if needed, required TIM1)
+//#define UART_EMUL_RX            PX_n // PinName used for RX
+//#define UART_EMUL_TX            PX_n // PinName used for TX
+
+// Default pin used for 'Serial' instance (ex: ST-Link)
+// Mandatory for Firmata
+#define PIN_SERIAL_RX           D12
+#define PIN_SERIAL_TX           D11
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+/*----------------------------------------------------------------------------
+ *        Arduino objects - C++ only
+ *----------------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+// These serial port names are intended to allow libraries and architecture-neutral
+// sketches to automatically default to the correct port name for a particular type
+// of use.  For example, a GPS module would normally connect to SERIAL_PORT_HARDWARE_OPEN,
+// the first hardware serial port whose RX/TX pins are not dedicated to another use.
+//
+// SERIAL_PORT_MONITOR        Port which normally prints to the Arduino Serial Monitor
+//
+// SERIAL_PORT_USBVIRTUAL     Port which is USB virtual serial
+//
+// SERIAL_PORT_LINUXBRIDGE    Port which connects to a Linux system via Bridge library
+//
+// SERIAL_PORT_HARDWARE       Hardware serial port, physical RX & TX pins.
+//
+// SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
+//                            pins are NOT connected to anything by default.
+#define SERIAL_PORT_MONITOR     Serial
+#define SERIAL_PORT_HARDWARE    Serial
+#endif
+
+#endif /* _VARIANT_ARDUINO_STM32_ */
